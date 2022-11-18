9c1e21a02acd6a3caa29aa935877f598c504bf2e
Dipper <1023128147@qq.com>
2022-11-19 00:02:52 +0800

    deploy

---
+++ precache-manifest.f81b3a489bcb8e3d488dd75979b362cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d9c571c94a5df7c59a",
+    "url": "/piggy-bank-website/css/app.831eb6f8.css"
+  },
+  {
+    "revision": "3950dd25c8e32dd2c622cae7cfd154e2",
+    "url": "/piggy-bank-website/index.html"
+  },
+  {
+    "revision": "c5d9c571c94a5df7c59a",
+    "url": "/piggy-bank-website/js/app.2ca4447c.js"
+  },
+  {
+    "revision": "4d66af5bb2771a7088f2",
+    "url": "/piggy-bank-website/js/chunk-vendors.5fef501b.js"
+  },
+  {
+    "revision": "7b5eebf621558fd72e48a75fa199d93d",
+    "url": "/piggy-bank-website/manifest.json"
+  },
+  {
+    "revision": "3a2d2369bbdb7e502515568ee2f03dab",
+    "url": "/piggy-bank-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/piggy-bank-website/robots.txt"
+  }
+]);
